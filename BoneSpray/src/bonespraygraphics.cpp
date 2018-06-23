6fd1df112cf3f4e4633d0d09c8bf2a09927e74c7
Chris Walker <chris@chriswalker.soy>
2018-06-23 19:35:24 +0100

    comments

--- BoneSpray/src/bonespraygraphics.cpp
+++ BoneSpray/src/bonespraygraphics.cpp
@@ -1,14 +1,14 @@
 #include "stdafx.h"
 #include "bonespraygraphics.h"
+
 #include "sload.h"
-
 #include <iostream>
-
 #include "GL/glew.h"
 #include "GL/freeglut.h"
 
 namespace bonegl
 {
+	// The program containing our shader registrations
 	GLuint program;
 
 	void render(void)
@@ -18,11 +18,23 @@ namespace bonegl
 		glUseProgram(program);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-	 
 		glutSwapBuffers();
 	}
 
-	void loadShaders()
+	/*
+	 *	Register event listeners against GLUT
+	 */
+	int registerListeners()
+	{
+		glutDisplayFunc(render);
+
+		return 0;
+	}
+
+	/*
+	 *	Reads the input shader files, and registers them with a Program for use by GL.
+	 */
+	int loadShaders()
 	{
 		SLoader sLoader;
 		program = sLoader.CreateProgram
@@ -30,17 +42,22 @@ namespace bonegl
 			"Shaders\\Vertex_Shader.glsl",
 			"Shaders\\Fragment_Shader.glsl"
 		);
+
+		return 0;
 	}
 
-	int launch(int argc, char **argv)
+	/*
+	 *	Initalise GLUT and GLEW, spawning our window and using our config.
+	 */
+	void init(int argc, char **argv)
 	{
 		// Config
 		const int GL_WINDOW_W = 500;
 		const int GL_WINDOW_H = 500;
 		const char* WINDOW_NAME = "Bone Spray";
-		const bool DISPLAY_FULL_SCREEN = false;
+		const bool DISPLAY_FULL_SCREEN = true;
 
-		// Initalise GLUT + GLEW
+		// Launch GLUT + window
 		glutInit(&argc, argv);
 		glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 		glutInitWindowSize(GL_WINDOW_W, GL_WINDOW_H);
@@ -48,16 +65,20 @@ namespace bonegl
 		glEnable(GL_DEPTH_TEST);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		glewInit();
+
 		if (DISPLAY_FULL_SCREEN) glutFullScreen();
+	}
 
-		// Bind Shaders
+	/*
+	 *	Launch OpenGL, register shaders and begin the render loop
+	 */
+	int launch(int argc, char **argv)
+	{
+		init(argc, argv);
 		loadShaders();
-
-		// Register Callbacks
-		glutDisplayFunc(render);
-
-		// Main Loop
+		registerListeners();
 		glutMainLoop();
+
 		return 0;
 	}
 }
