399371f8a49b0009917e33727e38508c00639327
Chris Walker <chris@chriswalker.soy>
2018-06-23 17:48:31 +0100

    Split Jack functionality out into seperate file

---
+++ BoneSpray/jackinterface.cpp
@@ -0,0 +1,53 @@
+
+#include "stdafx.h"
+#include "jack/jack.h"
+#include "jack/types.h"
+
+namespace jint
+{
+	jack_port_t *in_port;
+
+	jack_client_t *client;
+
+	int process(jack_nframes_t nframes, void *arg) {
+		jack_default_audio_sample_t *in;
+		int i;
+
+		in = (jack_default_audio_sample_t *) jack_port_get_buffer(in_port, nframes);
+		
+		for (i = 0; i < nframes; i++)
+		{
+			// process
+		}
+
+		return 0;
+	}
+
+	void shutdown(void *arg)
+	{
+		exit(1);
+	}
+
+	void activate()
+	{
+		const char *CLIENT_NAME = "BS";
+		const char *IN_NAME = "BSIn1";
+		jack_status_t *STATUS = {};
+
+		client = jack_client_open(CLIENT_NAME, JackNullOption, STATUS);
+		in_port = jack_port_register(client, IN_NAME, JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+
+		jack_nframes_t nframes = jack_get_buffer_size(client);
+
+		jack_set_process_callback(client, process, 0);
+		jack_on_shutdown(client, shutdown, 0);
+
+		int activate = jack_activate(client);
+	}
+
+	void close()
+	{
+		jack_client_close(client);
+		exit(0);
+	}
+}
---
+++ BoneSpray/jackinterface.h
@@ -0,0 +1,13 @@
+#ifndef DEFINE_JINT_H
+#define DEFINE_JINT_H
+
+#include "stdafx.h"
+
+namespace jint
+{
+	void activate();
+
+	void close();
+};
+
+#endif
